fd810db0fa5ec2b60dea8e957be4d52e0f6e5c7f
elvisun <elvissunlihe@gmail.com>
2017-03-21 17:08:13 -0400

    implement task logging and display

--- logging.c
+++ logging.c
@@ -8,7 +8,7 @@
 #define	ON						1				// Both experimental, and development board work on an active low configuration
 #define	OFF					0				// Both experimental, and development board work on an active low configuration
 #define	N_TASKS				4				// Number of tasks
-
+#define  DATAARRAYSIZE     90          // size for message Q
 
 
 // Redefine uC/OS-II configuration constants as necessary
@@ -32,6 +32,8 @@ char	TMRB_LSB;									// this 8-bit value will be written to TBL2R (bits 0 to 7
 int	PulseWidth;                         // Duty Cycle of the PWM signal
 float	PotNorm;										// Scaled value of the POT reading
 
+int tubeHeight;
+int ballXPos, ballYPos;
 
 char TMRB_10_count;                       // This variable is incrimented at the begining of every PWM cycle. When it is equal to ten, it is
                                           // reset to zero, and duty cycle values are calculated from updated ADC input.
@@ -40,6 +42,8 @@ char TMRB_10_count;                       // This variable is incrimented at the
 
 UBYTE			TaskData[N_TASKS];      		// Parameters to pass to each task
 OS_EVENT		*ADCSem;								// Semaphore to access ADC
+OS_EVENT    *loggingSem;
+OS_EVENT    *messageQ;
 
 void  InitializeTimers();						// Setup Timer A and B interrupts
 void  CalculateDutyCycle();               // Update the duty cycle
@@ -49,8 +53,8 @@ void  TaskInput(void *data);					// Function prototypes of the task
 void	TaskControl (void *data);				// Function prototypes of the task
 void  TaskStart(void *data);              // Function prototype of startup task
 void  DispStr(int x, int y, char *s);
-//void  TaskLogging (void *data) ;          // Function to print out the position
-
+void  TaskLogging (void *data) ;          // Function to print out the position
+void *dataArray[DATAARRAYSIZE];           // Q pointer storage array
 
 void main (void)
 {
@@ -58,7 +62,8 @@ void main (void)
    brdInit();                             // Initialize MCU baord
    OSInit();                              // Initialize uC/OS-II
    ADCSem = OSSemCreate(1);               // Semaphores for ADC inputs
-
+   loggingSem = OSSemCreate(0);           // 0 since using as flag
+   messageQ = OSQCreate(&dataArray[0],  DATAARRAYSIZE);
    OSTaskCreate(TaskStart, (void *)0, TASK_STK_SIZE, 10);
    OSStart();                             // Start multitasking
 }
@@ -68,7 +73,10 @@ void TaskStart (void *data)
    OSStatInit();
    OSTaskCreate(TaskInput, (void *)&TaskData[1], TASK_STK_SIZE, 11);
    OSTaskCreate(TaskControl, (void *)&TaskData[2], TASK_STK_SIZE, 5);
+   OSTaskCreate(TaskLogging, (void *)&TaskData[3], TASK_STK_SIZE, 12);
    InitializeTimers();							// Setup timer A and B interrupts
+
+   tubeHeight = 30;
 
    DispStr(0, 10, "Analog Input Reading: xxxx  ");
    DispStr(0, 13, "#Tasks          : xxxxx  CPU Usage: xxxxx   %");
@@ -98,64 +106,107 @@ nodebug void TaskInput (void *date)
    	DispStr(22, 10, display);
 
 
-	if (PotRead >= PotMax && PotRead <= PotMin)
-		PotNorm = ((float)PotRead-(float)PotMax)/((float)PotMin-(float)PotMax);
-	else if (PotRead < PotMax)
-		PotNorm = 0;
-	else if (PotRead > PotMin)
-		PotNorm = 1;
+   	if (PotRead >= PotMax && PotRead <= PotMin)
+   		PotNorm = ((float)PotRead-(float)PotMax)/((float)PotMin-(float)PotMax);
+   	else if (PotRead < PotMax)
+   		PotNorm = 0;
+   	else if (PotRead > PotMin)
+   		PotNorm = 1;
 
       OSTimeDly(OS_TICKS_PER_SEC);
-      }
+   }
 }
 
 
-// nodebug void TaskLogging (void *data){
-//     auto UBYTE err;
-//     char display[64];
-//     int i;    // counter variable for loop
-//     for(;;){
-//         // wait for the semaphore from the task control. Once given, print data.
-//         OSSemPend(loggingSem, 0, &err);
-        
-//         for(i = 0; i < arraySize; i++) {
+nodebug void TaskLogging (void *data){
+   float p, i, d, ir, pv, es;
+   int i, initialRow;    // counter variable for loop
+   initialRow = 30;
+   for(;;){
+      
+      // p, i, d, ir, pot value, error signal         6 values
+      p = OSQPend(messageQ, 0, &err);
+      i = OSQPend(messageQ, 0, &err);
+      d = OSQPend(messageQ, 0, &err);
+      ir = OSQPend(messageQ, 0, &err);
+      pv = OSQPend(messageQ, 0, &err);
+      es = OSQPend(messageQ, 0, &err);
 
-//             sprintf(display, "%f", proportionalArr[i]);
-//             DispStr(0, 35+i, display);
+      sprintf(display, "%f", p);
+      DispStr(0, initialRow + i , display);
+      sprintf(display, "%f", i);
+      DispStr(10, initialRow + i , display);
+      sprintf(display, "%f", d);
+      DispStr(20, initialRow + i , display);
 
-//         }
-//     }
-// }
+      sprintf(display, "%f", ir);
+      DispStr(30, initialRow + i , display);
+      sprintf(display, "%f", pv);
+      DispStr(40, initialRow + i , display);
+      sprintf(display, "%f", es);
+      DispStr(50, initialRow + i , display);
+      i++;
+
+      //free running loop, runs whenever CPU is free
+    }
+}
+
 
 nodebug void TaskControl (void *data)
 {
    auto UBYTE err;
-   int IrSen;
+   int IrSen counter;
    float IrNorm, ErrSig, MIN_PW, KP;
+   float dataStorage[90]; 
+   counter = 0;
    MIN_PW = 0.6;
    KP = 0.7;
    for (;;) {
 
-   OSSemPend(ADCSem,0,&err);
-   IrSen = anaIn(IRS_CHAN);
-   OSSemPost(ADCSem);
-   if (IrSen >= IrVmax && IrSen <= IrVmin)
-		IrNorm = ((float)IrSen-(float)IrVmax)/((float)IrVmin-(float)IrVmax);
-	else if (IrSen < IrVmax)
-		IrNorm = 0;
-	else if (IrSen > IrVmin)
-		IrNorm = 1;
-   ErrSig = PotNorm-IrNorm;
-   PulseWidth = (int)((MIN_PW - KP*ErrSig)*MAX_PWIDTH);
-   if (PulseWidth > MAX_PWIDTH){
-   	PulseWidth = MAX_PWIDTH;}
-   else if (PulseWidth < 0){
-   	PulseWidth = 0;}
-   TMRB_LSB = (char)PulseWidth;
-	TMRB_MSB = (char)(PulseWidth >> 2);
+      OSSemPend(ADCSem,0,&err);
+      IrSen = anaIn(IRS_CHAN);
+      OSSemPost(ADCSem);
+      if (IrSen >= IrVmax && IrSen <= IrVmin)
+   		IrNorm = ((float)IrSen-(float)IrVmax)/((float)IrVmin-(float)IrVmax);
+   	else if (IrSen < IrVmax)
+   		IrNorm = 0;
+   	else if (IrSen > IrVmin)
+   		IrNorm = 1;
+      ErrSig = PotNorm-IrNorm;
+      PulseWidth = (int)((MIN_PW - KP*ErrSig)*MAX_PWIDTH);
+      if (PulseWidth > MAX_PWIDTH){
+      	PulseWidth = MAX_PWIDTH;}
+      else if (PulseWidth < 0){
+      	PulseWidth = 0;}
+      TMRB_LSB = (char)PulseWidth;
+   	TMRB_MSB = (char)(PulseWidth >> 2);
+
+
+      //change ball y position to graph 
+      ballYPos = (int)((IrNorm * tubeHeight));
+
+      // p, i, d, ir, pot value, error signal         6 values
+      dataStorage[counter] = kp;
+      dataStorage[counter+1] = ki;
+      dataStorage[counter+2] = kd;
+      dataStorage[counter+3] = IrSen;
+      dataStorage[counter+4] = PotNorm;
+      dataStorage[counter+5] = ErrSig;
+      
+      OSQPost(messageQ, *dataStorage[counter]);        // are we passing by value???
+      OSQPost(messageQ, *dataStorage[counter+1]);
+      OSQPost(messageQ, *dataStorage[counter+2]);
+      OSQPost(messageQ, *dataStorage[counter+3]);
+      OSQPost(messageQ, *dataStorage[counter+4]);
+      OSQPost(messageQ, *dataStorage[counter+5]);
+
+      counter += 6;
+      if (counter > 89){
+         counter = 0;
+      }
 
       OSTimeDly(1);									// Delay the task
-      }
+   }
 }
 
 
@@ -232,4 +283,29 @@ void DispStr (int x, int y, char *s)
 }
 
 
+void DisplayTower ()
+{
+    auto int i;
+    const int XX, YY;
+    ballXPos = 72;
+    XX = 70;
+    YY = 5;
+    DispStr  (XX,YY,"=======");
+    for(i=0; i < tubeHeight; i++)
+        DispStr (XX,YY+1+i,"| |");
+    DispStr (XX,YY+1+i,"=======");
+    DispStr(ballXPos, YY + 1 + i - ballYPos, "O");
+}
 
+// =====
+// |   |
+// |   |
+// |   |
+// |   |
+// |   |
+// | 0 |
+// |   |
+// |   |
+// |   |
+// |   |
+// =====
